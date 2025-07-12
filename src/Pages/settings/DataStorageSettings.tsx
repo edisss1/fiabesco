ae1d3a4bd52a0a22c47f45cbc037e57ad1d51bcc
Sergey <kkotovic14@gmail.com>
2025-07-13 02:01:45 +0700

    update: added user data download

--- src/Pages/settings/DataStorageSettings.tsx
+++ src/Pages/settings/DataStorageSettings.tsx
@@ -1,11 +1,15 @@
 import Button from "../../components/atoms/Button"
+import { downloadUserData } from "../../utils/downloadUserData"
 
 const DataStorageSettings = () => {
     return (
         <div className="flex flex-col gap-9 min-w-[350px]">
             <div className="flex items-center justify-between">
                 <p>Download account data</p>
-                <Button className="rounded-lg border-2 dark:border-text-primary-dark hover:border-transparent cursor-pointer hover:bg-secondary transition-colors duration-200 border-text-primary px-6 py-2">
+                <Button
+                    onClick={downloadUserData}
+                    className="rounded-lg border-2 dark:border-text-primary-dark hover:border-transparent cursor-pointer hover:bg-secondary transition-colors duration-200 border-text-primary px-6 py-2"
+                >
                     Download
                 </Button>
             </div>
---
+++ src/utils/downloadUserData.ts
@@ -0,0 +1,24 @@
+import { api } from "../services/api"
+
+export const downloadUserData = async () => {
+    try {
+        const res = await api.get("/settings/data")
+
+        const data = res.data
+
+        const jsonString = JSON.stringify(data, null, 2)
+
+        const blob = new Blob([jsonString], { type: "application/json" })
+
+        const url = URL.createObjectURL(blob)
+
+        const link = document.createElement("a")
+        link.href = url
+        link.download = "data.json"
+        link.click()
+        link.remove()
+        URL.revokeObjectURL(url)
+    } catch (error) {
+        console.error(error)
+    }
+}
